8b5a11eacb673551c1a2e6d6be82dfb3fc255cd1
vtpl1 <monotosh.das@videonetics.com>
2017-08-05 17:14:56 +0530

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -4,34 +4,37 @@ from conans import ConanFile, CMake, tools
 class FfmpegConan(ConanFile):
     name = "ffmpeg"
     version = "3.3.3"
-    license = "<Put the package license here>"
-    url = "<Package recipe repository url here, for issues about the package>"
+    description = "Recipe for ffmpeg library"
+    license = "MIT/X Consortium license. Check file COPYING of the library"
+    url = "https://github.com/vtpl1/conan-ffmpeg"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
 
     def source(self):
-        self.run("git clone https://github.com/memsharded/hello.git")
-        self.run("cd hello && git checkout static_shared")
-        # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
-        # if the packaged project doesn't have variables to set it properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)", '''PROJECT(MyHello)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        self.run("git clone --depth 1 --branch release/3.3 https://github.com/FFmpeg/FFmpeg.git")
+        self.run("cd FFmpeg")
 
     def build(self):
-        cmake = CMake(self)
+
         self.run('cmake hello %s' % cmake.command_line)
-        self.run("cmake --build . %s" % cmake.build_config)
+        self.run("./configure --pkg-config-flags="--static" --extra-cflags="-I$HOME/ffmpeg_build/include" 
+                 --extra-ldflags="-L$HOME/ffmpeg_build/lib"
+                 --bindir="$HOME/bin"
+                 --enable-gpl
+                 --enable-libass
+                 --enable-libfdk-aac
+                 --enable-libfreetype
+                 --enable-libtheora                 
+                 --enable-nonfree")
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="ffmpeg")
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["ffmpeg"]
