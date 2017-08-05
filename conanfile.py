da59d1e3d8493b4d39a21547678a669c8f3081ab
vtpl1 <monotosh.das@videonetics.com>
2017-08-05 23:39:57 +0530

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,62 +1,52 @@
-from conans import ConanFile, tools, VisualStudioBuildEnvironment
-from conans.tools import download, unzip
-import os
-import shutil
+from conans import ConanFile, CMake, tools
 
-class FfmpegConan(ConanFile):
-    name = "ffmpeg"
-    version = "3.3.3"
-    description = "Recipe for ffmpeg library"
+
+class ExpatConan(ConanFile):
+    name = "Expat"
+    version = "2.2.1"
+    description = "Recipe for Expat library"
     license = "MIT/X Consortium license. Check file COPYING of the library"
-    url = "https://github.com/vtpl1/conan-ffmpeg"
-    settings = {"os": ["Windows", "Linux"], 
-                "compiler" : ["Visual Studio", "gcc"], 
-                "build_type" : ["Release", "Debug"], 
-                "arch"}
+    url = "https://github.com/vtpl1/conan-expat"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     generators = "cmake"
-    
-    def run_bash(self, cmd):
-        if self.settings.os == "Windows":
-            tools.run_in_windows_bash(self, cmd)
-        else:
-            self.run(cmd)
+    exports = ['FindExpat.cmake']
 
     def source(self):
-        zip_name = "ffmpeg.zip"
-        #download("https://codeload.github.com/FFmpeg/FFmpeg/zip/n%s.zip" % self.version, zip_name)
-        download("https://github.com/FFmpeg/FFmpeg/archive/n%s.zip" % self.version, zip_name)
-        unzip(zip_name)
-        shutil.move("FFmpeg-n%s" % self.version, "ffmpeg")
-        os.unlink(zip_name)
-        if self.settings.os=="Linux":
-            self.run_bash("chmod +x ffmpeg/configure")
-            self.run_bash("find ffmpeg -name '*.sh' -exec chmod +x {} \;");
-        
+        self.run("git clone --depth 1 --branch R_2_2_1 https://github.com/libexpat/libexpat")
+        # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
+        # if the packaged project doesn't have variables to set it properly
+        tools.replace_in_file("libexpat/expat/CMakeLists.txt", "project(expat)",
+            '''project(expat)
+            include(${CMAKE_BINARY_DIR}/../conanbuildinfo.cmake)
+            conan_basic_setup()''')
+
     def build(self):
-        with tools.chdir("ffmpeg") :
-            configure_cmd = "./configure --enable-nvenc --enable-pic --enable-cuvid --enable-asm --enable-yasm"
-            configure_cmd += " --disable-ffserver --disable-doc"
-            configure_cmd += " --disable-bzlib --disable-iconv --disable-zlib"
-            if self.settings.arch=="x86_64":
-                configure_cmd += " --arch=amd64"
-            if self.settings.os=="Windows":
-                configure_cmd += " --toolchain=msvc"
-            self.run_bash(configure_cmd)
-            self.run_bash("make")
+        cmake = CMake(self, parallel=True)
+
+        cmake_args = { "CMAKE_INSTALL_PREFIX" : self.package_folder,
+                       "BUILD_doc" : "OFF",
+                       "BUILD_examples" : "OFF",
+                       "BUILD_shared" : self.options.shared,
+                       "BUILD_tests" : "OFF",
+                       "BUILD_tools" : "OFF",
+                       #"CMAKE_CXX_FLAGS" : "${CMAKE_CXX_FLAGS} -DXML_POOR_ENTROPY"
+                     }
+
+        cmake.configure(source_dir="../libexpat/expat", build_dir="build", defs=cmake_args)
+        cmake.build(target="install")
+
     def package(self):
-        self.copy("*.h", dst="include/libavcodec", src="ffmpeg/libavcodec")
-        self.copy("*.h", dst="include/libavfilter", src="ffmpeg/libavfilter")
-        self.copy("*.h", dst="include/libavformat", src="ffmpeg/libavformat")
-        self.copy("*.h", dst="include/libavutil", src="ffmpeg/libavutil")
-        self.copy("*.h", dst="include/libavcodec", src="ffmpeg/libavcodec")
-        self.copy("*.h", dst="include/libswresample", src="ffmpeg/libswresample")
-        self.copy("*.h", dst="include/libswscale", src="ffmpeg/libswscale")
-        self.copy("*.lib", dst="lib", src="ffmpeg", keep_path=False)
-        self.copy("*-*.dll", dst="bin", src="ffmpeg", keep_path=False)
-        self.copy("*.so", dst="lib", src="ffmpeg", keep_path=False)
-        self.copy("*.so.*", dst="lib", src="ffmpeg", keep_path=False)
-        self.copy("*.a", dst="lib", src="ffmpeg", keep_path=False)
+        self.copy("FindExpat.cmake", ".", ".")
 
     def package_info(self):
-        self.cpp_info.libs = [ "avcodec", "avfilter", "avformat",
-                               "avutil", "swresample", "swscale" ]
+        self.cpp_info.debug.libs = ["expatd"]
+        self.cpp_info.release.libs = ["expat"]
+        self.cpp_info.libdirs = ["lib"]
+        self.cpp_info.includedirs = ["include"]
+        if not self.options.shared:
+            self.cpp_info.defines = ["XML_STATIC"]
+
+    def configure(self):
+        del self.settings.compiler.libcxx
