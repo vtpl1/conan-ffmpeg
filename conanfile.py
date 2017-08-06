92ea64a104c83290f52eb147632e7a4db985b45c
vtpl1 <monotosh.das@videonetics.com>
2017-08-06 14:59:26 +0530

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -14,13 +14,13 @@ class FfmpegConan(ConanFile):
                 "build_type" : ["Release", "Debug"], 
                 "arch" : ["x86", "x86_64"]}
     generators = "cmake"
-    
+
     def run_bash(self, cmd):
         if self.settings.os == "Windows":
             tools.run_in_windows_bash(self, cmd)
         else:
             self.run(cmd)
-    
+
     def source(self):
         zip_name = "ffmpeg.zip"
         #download("https://codeload.github.com/FFmpeg/FFmpeg/zip/n%s.zip" % self.version, zip_name)
@@ -31,7 +31,7 @@ class FfmpegConan(ConanFile):
         if self.settings.os=="Linux":
             self.run_bash("chmod +x ffmpeg/configure")
             self.run_bash("find ffmpeg -name '*.sh' -exec chmod +x {} \;");
-
+        
     def build(self):
         with tools.chdir("ffmpeg") :
             configure_cmd = "./configure --enable-nvenc --enable-pic --enable-cuvid --enable-asm --enable-yasm"
@@ -43,3 +43,20 @@ class FfmpegConan(ConanFile):
                 configure_cmd += " --toolchain=msvc"
             self.run_bash(configure_cmd)
             self.run_bash("make")
+    def package(self):
+        self.copy("*.h", dst="include/libavcodec", src="ffmpeg/libavcodec")
+        self.copy("*.h", dst="include/libavfilter", src="ffmpeg/libavfilter")
+        self.copy("*.h", dst="include/libavformat", src="ffmpeg/libavformat")
+        self.copy("*.h", dst="include/libavutil", src="ffmpeg/libavutil")
+        self.copy("*.h", dst="include/libavcodec", src="ffmpeg/libavcodec")
+        self.copy("*.h", dst="include/libswresample", src="ffmpeg/libswresample")
+        self.copy("*.h", dst="include/libswscale", src="ffmpeg/libswscale")
+        self.copy("*.lib", dst="lib", src="ffmpeg", keep_path=False)
+        self.copy("*-*.dll", dst="bin", src="ffmpeg", keep_path=False)
+        self.copy("*.so", dst="lib", src="ffmpeg", keep_path=False)
+        self.copy("*.so.*", dst="lib", src="ffmpeg", keep_path=False)
+        self.copy("*.a", dst="lib", src="ffmpeg", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = [ "avcodec", "avfilter", "avformat",
+                               "avutil", "swresample", "swscale" ]
